6d2431d2d9d484e8a5e0d6547e50da7a51a986fd
louellrmnn <ellermannlouise@gmail.com>
2023-12-11 11:27:34 +0100

    campaign

--- app.js
+++ app.js
@@ -3,22 +3,30 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  
+app.use('/api/campaign', (req, res, next) => {
+    const campaign = [
+      {
+        name: 'campagne1',
+        description: 'Les infos de ma première campagne',
+        startDate: '2023-01-01',
+        endDate: '2023-02-01',
+        budget: 4900,
+      },
+      {
+        name: 'campagne2',
+        description: 'Les infos de ma première campagne',
+        startDate: '2023-01-01',
+        endDate: '2023-02-01',
+        budget: 6500,
+      },
+    ];
+    res.status(200).json(campaign);
+  });
 
 module.exports = app;
